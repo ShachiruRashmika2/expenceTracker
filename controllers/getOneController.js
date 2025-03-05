3920188d71099a829758406ead7000753e5d1696
ShachiruRashmika2 <it22602046@my.sliit.lk>
2025-03-06 04:03:57 +0530

    initiate sample CRUD

--- controllers/getOneController.js
+++ controllers/getOneController.js
@@ -2,3 +2,15 @@ exports.getOne=(req,res)=>{
 
     res.send("Get one Modified");
 }
+exports.makeOne=(req,res)=>{
+
+    res.status(200).json({masage:"Created"});
+}
+exports.updateOne=(req,res)=>{
+
+    res.status(200).json({massage:`Updated ${req.params.id}`});
+}
+exports.deleteOne=(req,res)=>{
+
+    res.status(200).json({massage:`Deleted ${req.params.id}`});
+}
--- routes/getOneRoute.js
+++ routes/getOneRoute.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const getOneController=require('../controllers/getOneController');
 
 router.get('/api/getone',getOneController.getOne);
+router.post('/api/getone',getOneController.makeOne);
+router.put('/api/getone/:id',getOneController.updateOne);
+router.delete('/api/getone/:id',getOneController.deleteOne);
 
 module.exports=router;
